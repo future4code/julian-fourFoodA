185889e86151b6af0215c3c9a85fb6dfa79ac219
Murilo Oliari Ribeiro <murilo.oliari@gmail.com>
2020-06-17 08:46:48 -0300

    alteração do regex para verificação de e-mail

--- src/Pages/LoginPage/index.js
+++ src/Pages/LoginPage/index.js
@@ -53,7 +53,7 @@ const LoginPage = () => {
   };
 
   const emailValidation = (email) => {
-    const rgx = /^[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$/;
+    const rgx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return rgx.test(String(email).toLowerCase());
   };
 
