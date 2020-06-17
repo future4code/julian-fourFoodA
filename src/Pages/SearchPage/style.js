f4b8d2dc8826f0135f711a7c5095def4dda79700
Hugo Lacerda <hugolacerdar@gmail.com>
2020-06-17 11:32:31 -0300

    Home faltando só alguma melhoria entre as mudanças de filtros, mas pronta para uso

--- src/Pages/SearchPage/style.js
+++ src/Pages/SearchPage/style.js
@@ -1,4 +1,26 @@
 import styled from 'styled-components';
+import {
+  TextField
+} from '@material-ui/core';
+
+export const EmptySearch = styled.div`
+font-style: Roboto;
+margin-top: 12px;
+text-align: center;
+font-size: 16px;
+`
 
 export const SearchPageContainer = styled.div`
+`
+
+export const SearchField = styled(TextField)`
+&&{
+  margin-top: 8px;
+  margin-bottom: 8px;
+  width: 100%;
+}
+`
+
+export const SearchContainer = styled.div`
+margin: 0 16px;
 `
